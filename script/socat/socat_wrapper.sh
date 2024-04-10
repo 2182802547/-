49afa9130db59ee25c0ed282e31430fc1d7e8bb4
Xiang Li <2182802547@qq.com>
2024-04-10 23:06:51 +0800

    Update socat_wrapper.sh

--- script/socat/socat_wrapper.sh
+++ script/socat/socat_wrapper.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #IPv4 forward
-/usr/bin/socat TCP4-LISTEN:443,fork TCP4:1.1.1.1:80 &
-/usr/bin/socat UDP4-LISTEN:443,fork UDP4:1.1.1.1:80 &
+#/usr/bin/socat TCP4-LISTEN:443,fork TCP4:1.1.1.1:80 &
+#/usr/bin/socat UDP4-LISTEN:443,fork UDP4:1.1.1.1:80 &
 
 #IPv6 forward
-/usr/bin/socat TCP4-LISTEN:443,fork TCP6:[2606:4700:4700::1111]:80 &
-/usr/bin/socat UDP4-LISTEN:443,fork UDP6:[2606:4700:4700::1001]:80 &
+/usr/bin/socat UDP4-LISTEN:38337,fork UDP6:[2400:c620:0011:0000:0000:0000:63ce:06c4]:38337 &
 
 wait
